ad6cfdc5bfafcf0a54829759c70ea7609c5993b7
salma1389 <charfi.salma9@gmail.com>
2022-01-29 21:46:16 +0100

    checkpoint state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    person: {
+      fullName: "Salma Charfi",
+      bio: "Electrical Engineer and Web developer",
+      imgSrc:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgDBbKbOznWlZlfp2uNoo3RqPb_MmZKGuk8A&usqp=CAU",
+      profession: "Engineer",
+    },
+    shows: true,
+    count:0,
+  };
+
+  componentDidMount=()=>{
+    setInterval(()=>{
+      this.setState({count:this.state.count+1}) 
+    }, 1000);//run this thang every 2 seconds
+
+  }
+
+
+  Handleshows = () => {
+    this.setState({ shows: !this.state.shows });
+  };
+  render() {
+    return (
+      <div className="App">
+        <div className="container">
+        <button className={this.state.shows?"buttonHide":"buttonShow"} onClick={this.Handleshows}> {this.state.shows?'Hide':"Show"} </button>
+        </div>
+        {this.state.shows ? (
+          <div >
+            <div className='counter'>
+            <h2> COUNTER </h2>
+            <div>
+            <button >{this.state.count}</button>
+            </div>
+            </div>
+            <h1> {this.state.person.fullName}</h1>
+            <h1> {this.state.person.bio} </h1>
+            <img src={this.state.person.imgSrc} alt="" />
+            <h1> {this.state.person.profession} </h1>
+          </div>
+        ) : null}
+      </div>
+    );
+  }
 }
-
-export default App;
